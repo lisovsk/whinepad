3786719377bc71aedbe680ed3ec91679633742a8
lisovskiy <d.lisovskiy@theomnicore.com>
2017-08-21 13:29:49 +0300

    v.0.0.1

--- js/source/app.js
+++ js/source/app.js
@@ -3,10 +3,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
+import Excel from './components/Excel';
+
+var headers = localStorage.getItem('headers');
+var data = localStorage.getItem('data');
+if (!headers) {
+headers = ['Title', 'Year', 'Rating', 'Comments'];
+data = [['Test', '2015', '3', 'meh']];
+}
 
 ReactDOM.render(
-  <h1>
-    <Logo /> Welcome to The App!
-  </h1>,
-  document.getElementById('app')
+  <div>
+    <h1>
+      <Logo /> Welcome to The App!
+    </h1>
+    <Excel headers={headers} initialData={data} />
+  </div>,
+  document.getElementById('pad')
 );
